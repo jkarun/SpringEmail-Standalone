9be5bbdb6b613e221b1eda4c989cdd0214db8de9
arun <jkarun63@gmail.com>
2017-03-31 18:31:25 +0530

    Oauth2 connection attempt

--- src/main/java/com/arun/config/EmailConfig.java
+++ src/main/java/com/arun/config/EmailConfig.java
@@ -2,6 +2,13 @@ package com.arun.config;
 
 import java.util.Properties;
 
+import javax.mail.Authenticator;
+import javax.mail.MessagingException;
+import javax.mail.NoSuchProviderException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Store;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -21,13 +28,21 @@ public class EmailConfig {
 	Environment env;
 	
 	@Bean
-	public JavaMailSender javaMailSender(){
+	public JavaMailSender javaMailSender() throws MessagingException{
 		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
 		mailSender.setHost(env.getProperty("email.host")); 
 		mailSender.setPort(Integer.parseInt(env.getProperty("email.port")));
 		mailSender.setUsername(env.getProperty("email.user"));
 		mailSender.setPassword(env.getProperty("email.password"));
 		mailSender.setJavaMailProperties(this.getEmailProperties());
+		/*Session session = Session.getInstance(getEmailProperties(),new Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(null, null);
+			}
+		});*/
+		//Store store = session.getStore("smtp");
+		//store.connect("smtp.gmail.com", env.getProperty("email.user"), "1028668030975-24nnjkhlmudvpmkicnocmhvc3i7vh2s5.apps.googleusercontent.com");
+		//mailSender.setSession(session);
 		return mailSender;
 	}
 	
@@ -39,11 +54,12 @@ public class EmailConfig {
 	} 
 	
 	private Properties getEmailProperties(){
-		Properties prop = new Properties();
-		prop.put("mail.smtp.starttls.enable", env.getProperty("mail.smtp.starttls.enable")); 
-//		prop.put("mail.smtp.startssl.enable", env.getProperty("mail.smtp.startssl.enable")); 
-		prop.put("mail.smtp.auth", env.getProperty("mail.smtp.auth"));
-		prop.put("mail.transport.protocol", env.getProperty("mail.transport.protocol"));
+		Properties prop = new Properties(); 
+		//prop.put("mail.imap.ssl.enable", "true");// required for Gmail
+		prop.put("mail.transport.protocol", env.getProperty("mail.transport.protocol")); // Use SMTP transport protocol
+		prop.put("mail.smtp.auth", env.getProperty("mail.smtp.auth"));// Use SMTP-AUTH to authenticate to SMTP server
+		//prop.put("mail.smtp.auth.mechanisms",env.getProperty("mail.smtp.auth.mechanisms")); //GMail requires OAuth to not be considered "low-security"
+		prop.put("mail.smtp.starttls.enable", env.getProperty("mail.smtp.starttls.enable")); // Use TLS to encrypt communication with SMTP server
 		prop.put("mail.debug", env.getProperty("mail.debug"));
 		return prop;
 	}
